04a463d16d670b9eae501dba1d39225cdedf1268
Rahul Agarwal <talktorahulagarwal@gmail.com>
2022-07-16 13:37:58 +0530

    Inject Configuration Settings into ItemRespository

---
+++ Settings/ServiceSettings.cs
@@ -0,0 +1,10 @@
+namespace Play.Catalog.Service.Settings
+{
+    public class ServiceSettings
+    {
+        public static string SettingName = "ServiceSettings";
+
+
+        public string ServiceName { get; init; }
+    }
+}
